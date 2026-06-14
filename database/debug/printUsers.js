2b67832dffd35d0550b7d4080e701d449ad1a281
William <williamsjolund88@gmail.com>
2026-06-14 23:20:42 +0200

    Clear all rep files

--- database/debug/printUsers.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(db) {
-    db.all("SELECT * FROM users ORDER BY roblox_id", [], (err, rows) => {
-        if (err) {
-            console.error("Query error:", err.message);
-            return;
-        }
-        console.log("Query results:", rows);
-    });
-}
--- misc/status.js
+++
@@ -1,8 +0,0 @@
-const status = Object.freeze({
-    verified: 1,
-    officerPerms: 2,
-    hicomPerms: 3,
-    adminPerms: 4
-});
-
-module.exports = { status };
