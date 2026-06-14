f914bf1e75b435732e9371b70cb7d4bf07b999ea
William <williamsjolund88@gmail.com>
2026-06-14 23:48:50 +0200

    removed gen files

--- dist/misc/status.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const status = Object.freeze({
-    verified: 1,
-    officerPerms: 2,
-    hicomPerms: 3,
-    adminPerms: 4
-});
-module.exports = { status };
